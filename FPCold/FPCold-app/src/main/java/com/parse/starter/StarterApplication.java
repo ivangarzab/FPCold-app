bf299c8e99002aeb03e1b2300833924aa3bdcbcc
ivangb6 <ivgarber92@hotmail.com>
2016-12-18 01:12:13 +0100

    Adding working project!

---
+++ FPCold/FPCold-app/src/main/java/com/parse/starter/StarterApplication.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright (c) 2015-present, Parse, LLC.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ */
+package com.parse.starter;
+
+import android.app.Application;
+import android.util.Log;
+
+import com.parse.Parse;
+import com.parse.ParseACL;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseUser;
+import com.parse.SaveCallback;
+
+
+public class StarterApplication extends Application {
+
+  @Override
+  public void onCreate() {
+    super.onCreate();
+
+    // Enable Local Datastore.
+    Parse.enableLocalDatastore(this);
+
+    // Add your initialization code here
+    Parse.initialize(new Parse.Configuration.Builder(getApplicationContext())
+					.applicationId("oXBUzU2b8IcKRVx5r2GS")
+					.clientKey(null)
+					.server("http://fpcold-app.herokuapp.com/parse/")
+					.build()
+	);
+
+	  ParseObject obj = new ParseObject("Testy");
+	  obj.put("t1", 1);
+	  obj.put("name", "Ivan");
+	  obj.saveInBackground(new SaveCallback() {
+		  @Override
+		  public void done(ParseException e) {
+			  Log.i("TRASH", "WORKED!");
+		  }
+	  });
+
+      ParseUser.enableAutomaticUser();
+    ParseACL defaultACL = new ParseACL();
+    // Optionally enable public read access.
+    // defaultACL.setPublicReadAccess(true);
+    ParseACL.setDefaultACL(defaultACL, true);
+  }
+}
