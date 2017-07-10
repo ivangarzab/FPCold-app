d0d8c116d25dc7be5ff0bed6fe2da96bfe3d6c09
Ivan Garza <ivangb6@gmail.com>
2017-07-10 18:15:53 +0200

    Start with Settings/User/Edit design & functionality.

--- FPCold/FPCold-app/src/main/java/com/parse/starter/SettingsUsers.java
+++ FPCold/FPCold-app/src/main/java/com/parse/starter/SettingsUsers.java
@@ -9,6 +9,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
@@ -16,10 +17,15 @@ import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.parse.FindCallback;
 import com.parse.ParseException;
 import com.parse.ParseObject;
+import com.parse.ParseQuery;
 import com.parse.ParseUser;
 import com.parse.SignUpCallback;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Sapphire on 6/12/2017.
@@ -30,9 +36,12 @@ public class SettingsUsers extends Fragment implements View.OnClickListener {
 	private View VIEW;
 	private char ACTION;
 	private Button add, edit, confirm;
-	public EditText name, passcode;
+	private EditText name, passcode;
 	private Spinner user, tier;
 	private TextView title;
+
+	private int user_id;
+	private List<ParseUser> users_list;
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -57,21 +66,45 @@ public class SettingsUsers extends Fragment implements View.OnClickListener {
 		tier = (Spinner)view.findViewById(R.id.usersTierSpinner);
 		title = (TextView)view.findViewById(R.id.usersInfoTitleTextView);
 
+		users_list = new ArrayList<>();
+
 		add.setOnClickListener(this);
 		edit.setOnClickListener(this);
 		confirm.setOnClickListener(this);
 
 		setAction(ACTION);
 
-		ArrayAdapter<CharSequence> users_adapter = ArrayAdapter.createFromResource(VIEW.getContext(),
-				R.array.users_add, R.layout.custom_spinner_item);
-		users_adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-		user.setAdapter(users_adapter);
-
 		ArrayAdapter<CharSequence> tiers_adapter = ArrayAdapter.createFromResource(VIEW.getContext(),
 				R.array.tiers_array, R.layout.custom_spinner_item);
 		tiers_adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 		tier.setAdapter(tiers_adapter);
+
+		user.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+			@Override
+			public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+				user_id = i;
+
+				if (i == 0) {
+					name.setText("");
+					passcode.setText("");
+					tier.setSelection(0);
+				}
+				else {
+					ParseUser u = users_list.get(i - 1);
+					name.setText(u.get("name").toString());
+					passcode.setText(u.getUsername());
+					int t = Integer.valueOf(u.get("tier").toString());
+					if (t == 0 || t == 1) {
+						tier.setSelection(1);
+					} else if (t == 2) {
+						tier.setSelection(2);
+					} else Log.i("TRASH", "WTF?");
+				}
+			}
+
+			@Override
+			public void onNothingSelected(AdapterView<?> adapterView) { }
+		});
 	}
 
 	@Override
@@ -94,12 +127,12 @@ public class SettingsUsers extends Fragment implements View.OnClickListener {
 			ACTION = c;
 			add.setBackgroundColor(getResources().getColor(R.color.colorSecondary));
 			edit.setBackgroundColor(getResources().getColor(R.color.colorBackgroundLight));
-/*
+
 			ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(VIEW.getContext(),
 					R.array.users_add, R.layout.custom_spinner_item);
 			adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 			user.setAdapter(adapter);
-			*/
+
 			user.setClickable(false);
 
 			name.setText("");
@@ -109,14 +142,12 @@ public class SettingsUsers extends Fragment implements View.OnClickListener {
 			confirm.setText("ADD");
 		}
 		else if (c == 'e') {
+			ACTION = c;
 			edit.setBackgroundColor(getResources().getColor(R.color.colorSecondary));
 			add.setBackgroundColor(getResources().getColor(R.color.colorBackgroundLight));
-/*
-			ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(VIEW.getContext(),
-					R.array.users_add, R.layout.custom_spinner_item);
-			adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-			user.setAdapter(adapter);
-			*/
+
+			QueryUsers query = new QueryUsers();
+			query.execute();
 			user.setClickable(true);
 
 			name.setText("");
@@ -141,12 +172,14 @@ public class SettingsUsers extends Fragment implements View.OnClickListener {
 			signup.execute();
 		}
 		else if (ACTION == 'e') {
+			//ServerUpdate update = new ServerUpdate();
+			//update.execute();
 			Toast.makeText(VIEW.getContext(), "EDIT USER", Toast.LENGTH_LONG).show();
 		}
 	}
 
 
-	public class ServerSignup extends AsyncTask<Void, Void, Void> {
+	private class ServerSignup extends AsyncTask<Void, Void, Void> {
 
 		private String n, p;
 		private int t;
@@ -154,7 +187,7 @@ public class SettingsUsers extends Fragment implements View.OnClickListener {
 		private ProgressDialog PD;
 
 		// Constructor method
-		public ServerSignup() {
+		ServerSignup() {
 			this.n = name.getText().toString();
 			this.p = passcode.getText().toString();
 			this.t = Integer.valueOf(tier.getSelectedItem().toString().substring(0, 1));
@@ -198,17 +231,73 @@ public class SettingsUsers extends Fragment implements View.OnClickListener {
 		}
 	}
 
-	public class ServerUser extends AsyncTask<Void, Void, Void> {
+
+	private class QueryUsers extends AsyncTask<Void, Void, Void> {
 
 		// ProgressDialog to show while performing
 		private ProgressDialog PD;
 
 		// Constructor method
-		public ServerUser() { }
+		QueryUsers() { }
 
 		@Override
 		protected void onPreExecute() {
 			super.onPreExecute();
+			//Showing progress dialog while accessing server
+			PD = ProgressDialog.show(getActivity(), "Fetching Users", "Please wait...",
+					false, false);
+		}
+
+		@Override
+		protected void onPostExecute(Void aVoid) {
+			super.onPostExecute(aVoid);
+
+			ArrayList<String> users_names = new ArrayList<>();
+			users_names.add("Select User");
+			for (ParseUser user : users_list) {
+				users_names.add(user.get("name").toString());
+			}
+			ArrayAdapter<String> adapter = new ArrayAdapter<>(VIEW.getContext(),
+					R.layout.custom_spinner_item, users_names);
+			adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+			user.setAdapter(adapter);
+			//Dismissing the progress dialog
+			PD.dismiss();
+		}
+
+		@Override
+		protected Void doInBackground(Void... params) {
+			ParseQuery<ParseUser> query = ParseUser.getQuery();
+			try {
+				users_list = query.find();
+			} catch (ParseException e) {
+				e.printStackTrace();
+			}
+
+			return null;
+		}
+	}
+
+
+	private class ServerUpdate extends AsyncTask<Void, Void, Void> {
+
+		private ParseUser current_user;
+		private String name, pass;
+		private int tier;
+
+		// ProgressDialog to show while performing
+		private ProgressDialog PD;
+
+		// Constructor method
+		ServerUpdate() {
+			current_user = HomeActivity.CURRENT_USER;
+		}
+
+		@Override
+		protected void onPreExecute() {
+			super.onPreExecute();
+
+			ParseUser.logOut();
 			//Showing progress dialog while accessing server
 			PD = ProgressDialog.show(getActivity(), "Updating User", "Please wait...",
 					false, false);
@@ -218,13 +307,22 @@ public class SettingsUsers extends Fragment implements View.OnClickListener {
 		protected void onPostExecute(Void aVoid) {
 			super.onPostExecute(aVoid);
 
+
+
 			//Dismissing the progress dialog
 			PD.dismiss();
 		}
 
 		@Override
 		protected Void doInBackground(Void... params) {
-
+			/*
+			try {
+				ParseUser.logIn(edit_user.getUsername(), edit_user.getUsername());
+			} catch (ParseException e) {
+				e.printStackTrace();
+			}
+			ParseUser current = ParseUser.getCurrentUser();
+*/
 			return null;
 		}
 	}
